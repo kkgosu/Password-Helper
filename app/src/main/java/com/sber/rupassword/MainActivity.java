a167e41270cf56ccccfcd48a16df63a6fdd76366
kkgosu <kostya96.08@mail.ru>
2019-09-28 16:14:46 +0300

    created second layout; copy system service; plurals

--- app/src/main/java/com/sber/rupassword/MainActivity.java
+++ app/src/main/java/com/sber/rupassword/MainActivity.java
@@ -1,19 +1,28 @@
 package com.sber.rupassword;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     private EditText mPasswordInput;
     private TextView mResultText;
+    private ImageButton mCopyButton;
 
-    private String[] russians;
-    private String[] latins;
+
+    private String[] mRussians;
+    private String[] mLatins;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +31,25 @@ public class MainActivity extends Activity {
 
         mPasswordInput = findViewById(R.id.password_input);
         mResultText = findViewById(R.id.text_result);
+        mCopyButton = findViewById(R.id.copy_password_button);
 
-        russians = getResources().getStringArray(R.array.russians);
-        latins = getResources().getStringArray(R.array.latins);
+        mRussians = getResources().getStringArray(R.array.russians);
+        mLatins = getResources().getStringArray(R.array.latins);
 
-        final PasswordHelper helper = new PasswordHelper(russians, latins);
+        final PasswordHelper helper = new PasswordHelper(mRussians, mLatins);
+
+        mCopyButton.setEnabled(false);
+        mCopyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                if (manager != null) {
+                    manager.setPrimaryClip(ClipData.newPlainText(
+                            getString(R.string.password), mResultText.getText().toString()));
+                    Toast.makeText(MainActivity.this, R.string.toast_text_copied, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         mPasswordInput.addTextChangedListener(new TextWatcher() {
             @Override
@@ -37,6 +60,7 @@ public class MainActivity extends Activity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 mResultText.setText(helper.convert(s));
+                mCopyButton.setEnabled(s.length() > 0);
             }
 
             @Override
