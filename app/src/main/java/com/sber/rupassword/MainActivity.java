5365e765b8f8f54920d46e3d486b40e739302490
kkgosu <kostya96.08@mail.ru>
2019-09-29 18:05:19 +0300

    created new layout; implemented generation password method

--- app/src/main/java/com/sber/rupassword/MainActivity.java
+++ app/src/main/java/com/sber/rupassword/MainActivity.java
@@ -7,55 +7,68 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements CompoundButton.OnCheckedChangeListener {
+
+    PasswordHelper helper;
 
     private EditText mPasswordInput;
     private TextView mResultText;
+    private TextView mResultGeneratedText;
+    private TextView mPasswordLength;
     private ImageButton mCopyButton;
-    private ImageView mComplexityImage;
+    private ImageButton mCopyButtonGenerated;
+    private ImageView mComplexityInput;
+    private ImageView mComplexityGenerated;
+    private SeekBar mSeekBar;
+    private CompoundButton mCaps;
+    private CompoundButton mSymbols;
+    private CompoundButton mNumbers;
 
     private String[] mRussians;
     private String[] mLatins;
 
+    private int MIN_QUANTITY = 4;
+    private int mSymbolsQuantity = MIN_QUANTITY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mPasswordInput = findViewById(R.id.password_input);
-        mResultText = findViewById(R.id.text_result);
-        mCopyButton = findViewById(R.id.copy_password_button);
-        mComplexityImage = findViewById(R.id.complexity_input);
-
-        for (int i = 0; i < 75; i++) {
-            mComplexityImage.getDrawable().setLevel(i);
-        }
+        setContentView(R.layout.activity_main_new);
 
         mRussians = getResources().getStringArray(R.array.russians);
         mLatins = getResources().getStringArray(R.array.latins);
+        helper = new PasswordHelper(mRussians, mLatins);
 
-        final PasswordHelper helper = new PasswordHelper(mRussians, mLatins);
+        mPasswordInput = findViewById(R.id.password_input);
+        mResultText = findViewById(R.id.text_result);
+        mResultGeneratedText = findViewById(R.id.text_result_generated);
+        mPasswordLength = findViewById(R.id.password_length);
+        mCopyButton = findViewById(R.id.copy_password_button);
+        mCopyButtonGenerated = findViewById(R.id.copy_password_generated_button);
+        mComplexityInput = findViewById(R.id.complexity_input);
+        mSeekBar = findViewById(R.id.seekbar);
+        mCaps = findViewById(R.id.check_caps);
+        mSymbols = findViewById(R.id.check_symbols);
+        mNumbers = findViewById(R.id.check_numbers);
+
+        mCaps.setOnCheckedChangeListener(this);
+        mSymbols.setOnCheckedChangeListener(this);
+        mNumbers.setOnCheckedChangeListener(this);
 
         mCopyButton.setEnabled(false);
-        mCopyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                if (manager != null) {
-                    manager.setPrimaryClip(ClipData.newPlainText(
-                            getString(R.string.password), mResultText.getText().toString()));
-                    Toast.makeText(MainActivity.this, R.string.toast_text_copied, Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        mCopyButton.setOnClickListener(v -> copyToClipboard(mResultText));
+        mCopyButtonGenerated.setOnClickListener(view -> copyToClipboard(mResultGeneratedText));
+
+        setPlurals();
+        generatePassword();
 
         mPasswordInput.addTextChangedListener(new TextWatcher() {
             @Override
@@ -74,5 +87,49 @@ public class MainActivity extends Activity {
             }
         });
 
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mSymbolsQuantity = MIN_QUANTITY + i;
+                setPlurals();
+                generatePassword();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        generatePassword();
+    }
+
+    private void generatePassword() {
+        mResultGeneratedText.setText(helper.generatePassword(mSymbolsQuantity, mCaps.isChecked(),
+                mSymbols.isChecked(), mNumbers.isChecked()));
+    }
+
+    private void copyToClipboard(TextView password) {
+        ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        if (manager != null) {
+            manager.setPrimaryClip(ClipData.newPlainText(
+                    getString(R.string.password), password.getText().toString()));
+            Toast.makeText(MainActivity.this, R.string.toast_text_copied, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setPlurals() {
+        String symbols = getResources().getQuantityString(
+                R.plurals.symbols_count, mSymbolsQuantity, mSymbolsQuantity);
+        mPasswordLength.setText(symbols);
     }
 }
--- app/src/main/java/com/sber/rupassword/PasswordHelper.java
+++ app/src/main/java/com/sber/rupassword/PasswordHelper.java
@@ -1,8 +1,15 @@
 package com.sber.rupassword;
 
+import java.util.Random;
+
 public class PasswordHelper {
     private final String[] russians;
     private final String[] latins;
+
+    private final String mEngCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final String mEng = "abcdefghijklmnopqrstuvwxyz";
+    private final String mNumbers = "0123456789";
+    private final String mSymbols = "!@#$%^&*_=+-/";
 
     public PasswordHelper(String[] russians, String[] latins) {
         this.russians = russians;
@@ -34,4 +41,23 @@ public class PasswordHelper {
 
         return result.toString();
     }
+
+    public String generatePassword(int length, boolean caps, boolean symbols, boolean numbers) {
+        StringBuilder resultPassword = new StringBuilder();
+        StringBuilder dictionary = new StringBuilder(mEng);
+        Random random = new Random();
+
+        if (caps)
+            dictionary.append(mEngCaps);
+        if (symbols)
+            dictionary.append(mSymbols);
+        if (numbers)
+            dictionary.append(mNumbers);
+
+        for (int i = 0; i < length; i++) {
+            resultPassword.append(dictionary.charAt(random.nextInt(dictionary.length())));
+        }
+        return resultPassword.toString();
+    }
+
 }
