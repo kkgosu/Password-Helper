936f051a07eec93efe17eef79fb6673d8c65627a
kkgosu <kostya96.08@mail.ru>
2020-01-27 23:43:37 +0300

    created master password activity creation; rewrote main_activity

---
+++ app/src/main/java/com/sber/rupassword/CreateMasterPassword.kt
@@ -0,0 +1,12 @@
+package com.sber.rupassword
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateMasterPassword : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_master_password)
+    }
+}
